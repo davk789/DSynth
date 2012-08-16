ff522cb39d09d0b2029f13460c96855c3bc360f3
davk789 <david.joseph.kendall@gmail.com>
2012-08-16 12:29:00 -0700

    Initial Commit

---
+++ DSynth/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DSynth
+//
+//  Created by David Kendall on 8/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DSynthTests/DSynthTests.h
@@ -0,0 +1,13 @@
+//
+//  DSynthTests.h
+//  DSynthTests
+//
+//  Created by David Kendall on 8/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DSynthTests : SenTestCase
+
+@end
