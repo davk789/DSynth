485968513b8c4160d9078e4fb9952bb7231dfc97
davk789 <david.joseph.kendall@gmail.com>
2012-08-17 18:59:15 -0700

    added readme

---
+++ DSynth/SynthManager.h
@@ -0,0 +1,13 @@
+//
+//  SynthManager.h
+//  DSynth
+//
+//  Created by David Kendall on 8/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SynthManager : NSObject
+
+@end
---
+++ DSynth/SynthManager.m
@@ -0,0 +1,13 @@
+//
+//  SynthManager.m
+//  DSynth
+//
+//  Created by David Kendall on 8/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SynthManager.h"
+
+@implementation SynthManager
+
+@end
